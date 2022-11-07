1b499544966e5fad9357a5e3c1754ee33c9425d5
e-mm-u <uh.habiba1012@gmail.com>
2022-11-07 23:45:36 +0600

    tailwind, daisyUI firebase installed, favicon added, .env created for firebase credential

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            
+        <div className="App text-primary text-danger">
+            <h1 className="text-red-800 text-5xl tracking-widest ">Hello world</h1>
+            <button className="btn btn-outline btn-info">Info</button>
+            <button className="btn btn-outline btn-success">Success</button>
+            <button className="btn btn-outline btn-warning">Warning</button>
+            <button className="btn btn-outline btn-error">Error</button>
         </div>
     );
 }
