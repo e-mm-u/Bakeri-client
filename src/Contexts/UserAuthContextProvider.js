06ac7bcf902fa88802192c4e73227ef18ba3e02f
e-mm-u <uh.habiba1012@gmail.com>
2022-11-08 01:02:36 +0600

    user context set up done

--- src/Contexts/UserAuthContextProvider.js
+++ src/Contexts/UserAuthContextProvider.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import {getAuth} from 'firebase/auth'
+import app from '../firebase/firebase.config';
 
-const UserAuthContextProvider = () => {
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const UserAuthContextProvider = ({children}) => {
+    const AuthInfo = { }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={AuthInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
