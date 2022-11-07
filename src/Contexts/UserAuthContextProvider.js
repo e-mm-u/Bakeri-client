7994bf1f78f712cbb9dcc892b47b650022ec0bd9
e-mm-u <uh.habiba1012@gmail.com>
2022-11-08 01:29:54 +0600

    login functionality added

--- src/Contexts/UserAuthContextProvider.js
+++ src/Contexts/UserAuthContextProvider.js
@@ -1,12 +1,31 @@
-import React, { createContext } from 'react';
-import {getAuth} from 'firebase/auth'
+import React, { createContext, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const UserAuthContextProvider = ({children}) => {
-    const AuthInfo = { }
+    const [user, setUser] = useState({});
+    const [loading, setloading] = useState(false);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleLogin = () => {
+        const provider = new GoogleAuthProvider();
+        return signInWithPopup(auth, provider);
+    }
+
+    const logout = () => {
+        return signOut(auth);
+    }
+
+    const AuthInfo = { user, loading, createUser, login, googleLogin, logout}
     return (
         <AuthContext.Provider value={AuthInfo}>
             {children}
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Contexts/UserAuthContextProvider';
 
 const Login = () => {
+    const { login, googleLogin } = useContext(AuthContext);
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        const user = { email, password, name };
+
+        login(email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+            })
+            .catch((error) => {
+                console.error(`errorCode : ${error.code} \nerrorMessage ${error.message}`)
+            });
+
+    }
+    const handleGoogleLogin = () => {
+        googleLogin()
+            .then((result) => {
+                const user = result.user;
+            })
+            .catch((error) => {
+                console.error(`errorCode : ${error.code} \nerrorMessage ${error.message}`)
+            });
+
+
+    }
     return (
         <div>
             Login
