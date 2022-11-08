b1ea1389518748d5a4ff26a6b21f9fa9b150a5e4
e-mm-u <uh.habiba1012@gmail.com>
2022-11-08 09:42:41 +0600

    navbar design fixed

--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/favicon.ico'
 
 const Header = () => {
     return (
         <div>
-            header
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><Link to='/'>Homepage</Link></li>
+                            <li><Link to='/services'>Services</Link></li>
+                            <li><Link to='/blogs'>Blog</Link></li>
+                        </ul>
+                    </div>
+                    <div className='flex align-center gap-1'>
+                        <div><img src={logo} alt="logo" /></div>
+                        <div><Link to='/' className="btn btn-ghost normal-case text-3xl p-0">BakerY</Link></div>
+                    </div>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/services'>Services</Link></li>
+                        <li><Link to='/blogs'>Blogs</Link></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <button className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="https://placeimg.com/80/80/people" alt='avater'/>
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><Link>Profile</Link></li>
+                            <li><Link>Settings</Link></li>
+                            <li><Link>Logout</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
