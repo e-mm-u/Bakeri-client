995b6d97570aeb3aac8b2c0968d73924707c316a
e-mm-u <uh.habiba1012@gmail.com>
2022-11-08 11:17:12 +0600

    header almost done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import routes from "./Routes/Routes";
 
 function App() {
     return (
-        <RouterProvider router={ routes }></RouterProvider>
+        <RouterProvider router={ routes }>
+            
+        </RouterProvider>
     );
 }
 
--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -4,7 +4,7 @@ import logo from '../../images/favicon.ico'
 
 const Footer = () => {
     return (
-        <div>
+        <>
             <footer className="footer p-10 bg-base-200 text-base-content">
                 <div>
                     <span className="footer-title">Services</span>
@@ -40,7 +40,7 @@ const Footer = () => {
                     </div>
                 </div>
             </footer>
-        </div>
+        </>
     );
 };
 
