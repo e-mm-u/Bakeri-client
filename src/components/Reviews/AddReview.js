6e80363c173f43db3eafb522c36c2dfba772a9c3
e-mm-u <uh.habiba1012@gmail.com>
2022-11-09 11:01:44 +0600

    POST-review(create) done

--- src/components/Reviews/AddReview.js
+++ src/components/Reviews/AddReview.js
@@ -1,44 +1,60 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/UserAuthContextProvider';
 
 const AddReview = ({ service }) => {
-
     const { user } = useContext(AuthContext);
-
-    const handleSubmit = event => {
-        event.preventDefault();
-
+    const [review, setReview] = useState({})
+    // ------------------ create and set review ---------
+    const createReview = event => {
+        const customer_review = event.target.value;
         const { email, displayName, photoURL, uid } = user;
         const { name, _id } = service;
 
-        const review = event.target.review.value;
-        const reviewInfo = {
-            review,
-            userInfo: {
-                email,
-                displayName,
-                photoURL,
-                uid
-            },
-            serviceInfo: {
-                name,
-                _id
-            }
-        };
-        console.log(reviewInfo);
+        const reviewInfo = { ...review };
+        reviewInfo['review'] = customer_review;
+        reviewInfo['userInfo'] = { email, displayName, photoURL, uid };
+        reviewInfo['serviceInfo'] = { name, _id, photoURL: service.photoURL };
+
+        console.log('before set and fetch post', reviewInfo);
+        setReview(reviewInfo);
+        console.log('after setting ', review);
     }
+
+    //  --------------- POST / add review ------------
+    const handleAddReview = event => {
+        event.preventDefault();
+        // ______________________________________
+        // ____________________ fetch - post ____
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('data from .then data addReview client', data)
+                if (data.acknowledged) {
+                    console.log('1 knwlg 1')
+                    alert('You have succesfully added a review')
+                    event.target.reset()
+                }
+            })
+    }
+
 
     return (
         <div>
             {user ?
                 <>
-                    <form onSubmit={handleSubmit} className="form-control">
+                    <form onSubmit={handleAddReview} className="form-control">
                         <div>
                             <label className="label">
                                 <span className="label-text">Write a review :</span>
                             </label>
-                            <textarea type="text" name='review' placeholder=" " className="textarea textarea-bordered h-24 w-full max-w-sm" required />
+                            <textarea onBlur={createReview} type="text" name='review' placeholder=" " className="textarea textarea-bordered h-24 w-full max-w-sm" required />
                         </div>
                         <div>
                             <input type="submit" value="Add" className='btn' />
