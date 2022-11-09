aeeb8105d1671d02d0f0b3ca74c631115b31a8b8
e-mm-u <uh.habiba1012@gmail.com>
2022-11-10 00:09:36 +0600

    blog page done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Blogs = () => {
+    const [blogs, setblogs] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/blogs')
+            .then(res => res.json())
+            .then(data => setblogs(data))
+    }, [])
     return (
-        <div>
-            Blogs
+        <div className='my-8 p-8'>
+            <h1 className='text-center text-3xl font-semibold uppercase'>Happy reading :) </h1>
+            {blogs.map(blog =>
+                <div className="collapse my-2">
+                    <input type="checkbox" className="peer" />
+                    <div className="collapse-title  bg-red-50 text-black peer-checked: bg-red-50 peer-checked:text-black">
+                        {blog.title}
+                    </div>
+                    <div className="collapse-content  bg-red-50 text-black peer-checked: bg-red-50 peer-checked:text-black">
+                        <h3>{blog.title}</h3>
+                        <p>{blog.details}</p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
